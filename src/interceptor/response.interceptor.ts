b20882eef20e254e307d9f26694d47fa5f5f7e3e
Aliasgar Pocketwala <pocketwala.ali@gmail.com>
2022-06-29 19:31:41 +0530

    feat: adding interceptors

---
+++ src/interceptor/response.interceptor.ts
@@ -0,0 +1,16 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, BadGatewayException } from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      catchError(err => {
+        console.log('Before...', err);
+        return throwError(() => new BadGatewayException());
+      }),
+      map(data => ({ data, new: true })),
+    );
+  }
+}
