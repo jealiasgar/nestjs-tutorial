da4b5890cf2a6382f86b4d865a837fedc480a26b
Aliasgar Pocketwala <pocketwala.ali@gmail.com>
2022-06-29 19:31:51 +0530

    feat: adding interceptors

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards, UseInterceptors } from '@nestjs/common';
 import { User } from '@prisma/client';
+import { TransformInterceptor } from '../../src/interceptor';
 import { GetUser } from '../auth/decorator/get-user.decorator';
 import { JwtGuard } from '../auth/guard';
 import { UpdateUserDto } from './dto';
 import { UserService } from './user.service';
 
+@UseInterceptors(TransformInterceptor)
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
